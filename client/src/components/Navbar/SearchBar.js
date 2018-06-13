f4ef492555760a0d1c81cfdbe0ad7c34541affe4
Trevor Meadows <tlm04070@gmail.com>
2018-06-12 21:05:22 -0400

    working on searching function

--- client/src/components/Navbar/SearchBar.js
+++ client/src/components/Navbar/SearchBar.js
@@ -1,11 +1,44 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "no results found",
+      showComponent: false
+    };
+    this.termSearch = this.termSearch.bind(this);
+    this.showDropdownMenu = this.showDropdownMenu.bind(this);
+  }
+  showDropdownMenu(event) {
+    event.preventDefault();
+    this.setState({ showComponent: true });
+  }
+
+  termSearch(event, term) {
+    event.preventDefault();
+    let inputTerm = event.target.value;
+    if (inputTerm === "testing") {
+      return this.setState({ term: inputTerm });
+    } else {
+      return this.setState({ term: "no results found" });
+    }
+  }
   render() {
     return (
       <div className="nav__boards--searchBar">
-        <input placeholder="Search for a board" />
-        <a className="nav__boards--searchButton">Search</a>
+        <input placeholder="Search for a board" onChange={this.termSearch} />
+        <a
+          className="nav__boards--searchButton"
+          onClick={this.showDropdownMenu}
+        >
+          Search
+        </a>
+        {this.state.showComponent ? (
+          <div>
+            <h1>{this.state.term}</h1>
+          </div>
+        ) : null}
       </div>
     );
   }
